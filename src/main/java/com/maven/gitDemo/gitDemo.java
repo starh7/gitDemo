76195bcf1405fb244364e158d9899ce38a67a759
he <he@qq.com>
2022-09-29 19:35:31 +0800

    third commit

--- src/main/java/com/maven/gitDemo/gitDemo.java
+++ src/main/java/com/maven/gitDemo/gitDemo.java
@@ -4,5 +4,6 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("git-demo");
         System.out.println("git-demo2");
+        System.out.println("git-demo3");
     }
 }
