e0ae398e24e269b580c42d19f75d44d9a5127e72
he <he@qq.com>
2022-09-29 19:35:01 +0800

    second commit

--- src/main/java/com/maven/gitDemo/gitDemo.java
+++ src/main/java/com/maven/gitDemo/gitDemo.java
@@ -3,5 +3,6 @@ package com.maven.gitDemo;
 public class gitDemo {
     public static void main(String[] args) {
         System.out.println("git-demo");
+        System.out.println("git-demo2");
     }
 }
